359c1e2a993c1c685f9e5cdc9e8f3ffdbcc7440d
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2026-02-05 12:42:55 -0500

    Cambios de environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,14 @@
-/* export const environment = {
+export const environment = {
   production: true,
   apiUrl: 'https://api.horariosap.com',
-}; */
+};
 
 /* export const environment = {
   production: false,
   apiUrl: 'http://localhost:3010',
 };
  */
-export const environment = {
+/* export const environment = {
   production: false,
   apiUrl: 'http://localhost:3011',
-};
+}; */
